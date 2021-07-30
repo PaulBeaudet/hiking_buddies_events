585eb08acc5e5bb96e90c9b39da9d6f7678f7a04
Paul Beaudet <inof8or@gmail.com>
2021-07-30 06:53:22 -0400

    upgrade to node 14

--- events.js
+++ events.js
@@ -1,29 +1,28 @@
 // events.js ~ Copyright 2019 Paul Beaudet ~ License MIT
-var querystring = require('querystring');  // Parse urlencoded body
-var crypto = require('crypto');            // verify request from slack is from slack with hmac-256
-var https = require('https');
+const crypto = require('crypto');            // verify request from slack is from slack with hmac-256
+const https = require('https');
 
-var slack = {
+const slack = {
     verify: function(event){
-        var timestamp = event.headers['X-Slack-Request-Timestamp'];        // nonce from slack to have an idea
-        var secondsFromEpoch = Math.round(new Date().getTime() / 1000);    // get current seconds from epoch because thats what we are comparing with
+        const timestamp = event.headers['X-Slack-Request-Timestamp'];        // nonce from slack to have an idea
+        const secondsFromEpoch = Math.round(new Date().getTime() / 1000);    // get current seconds from epoch because thats what we are comparing with
         if(Math.abs(secondsFromEpoch - timestamp > 60 * 5)){return false;} // make sure request isn't a duplicate
-        var computedSig = 'v0=' + crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET).update('v0:' + timestamp + ':' + event.body).digest('hex');
+        const computedSig = 'v0=' + crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET).update('v0:' + timestamp + ':' + event.body).digest('hex');
         return crypto.timingSafeEqual(Buffer.from(event.headers['X-Slack-Signature'], 'utf8'), Buffer.from(computedSig ,'utf8'));
     },
     send: function(msg, webhook){
-        var postData = JSON.stringify({'text': msg});
-        var options = {
+        const postData = JSON.stringify({'text': msg});
+        const options = {
             hostname: 'hooks.slack.com', port: 443, method: 'POST',
             path: webhook ? webhook : process.env.LOG_WH,
             headers: {'Content-Type': 'application/json','Content-Length': postData.length}
         };
-        var req = https.request(options, function(res){}); // just do it, no need for response
+        const req = https.request(options, function(res){}); // just do it, no need for response
         req.on('error', function(error){console.log(error);});
         req.write(postData); req.end();
     },
     handler: function(event, context, callback){
-        var response = {statusCode:403, headers: {'Content-type': 'application/json'}};
+        const response = {statusCode:403, headers: {'Content-type': 'application/json'}};
         if(slack.verify(event)){
             response.statusCode = 200;
             try{event.body = JSON.parse(event.body);}catch(error){console.log(error); callback(null, response);}
@@ -36,7 +35,7 @@ var slack = {
             } else {slack.send('unhandled event type: ' + JSON.stringify(event.body)); callback(null, response);}
         } else {slack.send('request to event handler not slack'); callback(null, response);}
     },
-    onTeamJoin: function(event, log){ // pass fuction on where to log (slack, cloudwatch, console, ect)
+    onTeamJoin: function(event, log){ // pass function on where to log (slack, cloudwatch, console, ect)
         slack.send('Welcome to hiking buddies slack '+event.user.real_name+'! (<@'+ event.user.id +
             '>) \nThis is a good channel to introduce yourself for those that don\'t know you.', process.env.NEW_MEMBERS_WH);
     }
